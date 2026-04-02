cf1d366f51d8f6eedd0c71fafe8f0bbb17a9f1de
doanlng <78599995+doanlng@users.noreply.github.com>
2026-04-01 20:34:51 -0400

    nvim

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,24 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    -- disable the keymap to grep files
+    { "<leader>/", false },
+    -- change a keymap
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    -- add a keymap to browse plugin files
+    {
+      "<leader>fp",
+      function()
+        require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+      end,
+      desc = "Find Plugin File",
+    },
+    {
+      "<leader>fg",
+      function()
+        require("telescope.builtin").live_grep()
+      end,
+      desc = "grep in curr directory.  Respects .gitignore",
+    },
+  },
+}
